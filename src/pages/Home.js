95aa6b4cb4358fa34a94dcb1159c267f4e597209
Miguel Nobre <mnobre24@gmail.com>
2023-02-02 03:22:43 -0500

    asd

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import "./Home.css"
 
 function Home(){
 
-  // const backend_url = "http://localhost:5000"
-  const backend_url = "http://F1fantasyapi-env.eba-w3fistyc.us-east-2.elasticbeanstalk.com"
+  const backend_url = "https://api.playf1fantasy.com"
+  // const backend_url = "http://f1fantasyapi-env.eba-w3fistyc.us-east-2.elasticbeanstalk.com"
   const [data, setData] = useState("")
   const didMount = useRef(false);
   // const [img, setImg] = useState();
